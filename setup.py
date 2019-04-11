f169b8e2085d18c16f63284a720455a61425f939
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2019-04-11 18:58:45 +0200

    Release v1.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 setup(
     name='inequality_coefficients',
     packages=['inequality_coefficients'],
-    version='1.1.1',
+    version='1.2.0',
     description='Coefficients to measure inequality.',
     long_description=long_description,
     long_description_content_type='text/markdown',
